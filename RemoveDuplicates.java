012ed24afc72b7cf25c1a09d26d4e0cfa4c59a41
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-16 21:22:14 +0530

    RemoveDuplicates

---
+++ RemoveDuplicates.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        for(int i = 0 ; i < n ; i++){
+            while(map.containsKey(arr[i])){
+                System.out.print(arr[i] + " ");
+                map.remove(arr[i]);
+            }
+
+        }
+    }
+}
