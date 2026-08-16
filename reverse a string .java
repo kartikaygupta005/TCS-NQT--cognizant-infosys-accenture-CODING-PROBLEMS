60d47cfbb0c6121eae7a28563ccb89e852ebc64f
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-16 20:08:07 +0530

    reverse a string

---
+++ reverse a string .java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String reversed = "";
+        for(int i = str.length()-1  ;  i >= 0 ; i-- ){
+            reversed = reversed + str.charAt(i);
+        }
+        System.out.print(reversed);
+    }
+}
