a8a9c18fc17a2a0b142d718e21bafb263a218ab8
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-16 20:49:54 +0530

    LargestAndSmallest

---
+++ LargestAndSmallest.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int smallest = arr[0];
+        int largest = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.print("largest " + largest + " smallest " + smallest);
+    }
+}
