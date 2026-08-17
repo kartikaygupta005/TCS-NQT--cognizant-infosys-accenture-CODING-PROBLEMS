251a8b78d764b8abbe492b5daa8ab2b93d0eff71
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-17 20:36:29 +0530

    circular linked list

---
+++ CirculaLinked List.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+// main algorithm is make a pointer which moves one step at a time and another pointer two steps at a time if they meet ever this means it is circular linked list 
