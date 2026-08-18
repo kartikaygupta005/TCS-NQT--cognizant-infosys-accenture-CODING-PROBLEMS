ff7430813afeb6c9a82c0770090f5800a9e5fef3
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-18 22:54:03 +0530

    Create Creating a linked list.java

---
+++ Creating a linked list.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < n; i++) {
+
+            int value = sc.nextInt();
+
+            Node newNode = new Node(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } 
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+    }
+}
